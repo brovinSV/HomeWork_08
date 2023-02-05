ee9c7209d02e6aff8fbce527044ff0d5dd27c285
Brovin Vyacheslav <brovinSV80@gmail.com>
2023-02-05 23:13:55 +0200

    Finish

--- py_charm.py
+++ py_charm.py
@@ -5,6 +5,8 @@ def mane_function():
     num2 = random.randint(5, 20)
     print(num1, " + ", num2, " = ")
     user_answer = int(input("Your answer: "))
+    actual_answer = num1 + num2
+
 def test_function():
     num3 = random.randint(25, 50)
     num4 = random.randint(1, 25)
