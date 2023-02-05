c8e4037115015c43e6052341df5f524eb9c3ce73
Brovin Vyacheslav <brovinSV80@gmail.com>
2023-02-05 23:07:18 +0200

    Add user_answer, actual_answer

--- py_charm.py
+++ py_charm.py
@@ -4,3 +4,7 @@ def test_function():
     num3 = random.randint(25, 50)
     num4 = random.randint(1, 25)
     print(num3, " - ", num4, " = ")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num3 - num4
+    answers = (user_answer, actual_answer)
+    return answers
