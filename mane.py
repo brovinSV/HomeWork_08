e268b6f6f0cfd853818b1d265c395d9f02f0e7ee
Brovin Vyacheslav <brovinSV80@gmail.com>
2023-02-05 22:51:01 +0200

    Add user_answer, actual_answer

--- mane.py
+++ mane.py
@@ -9,4 +9,7 @@ def mane_function():
 def test_function():
     num3 = random.randint(25, 50)
     num4 = random.randint(1, 25)
+    print(num3, " - ", num4, " = ")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num3 - num4
     
