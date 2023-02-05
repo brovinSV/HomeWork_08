14f2c6f8ff77eb0766e5ed4793c15c4593cef3cb
Brovin Vyacheslav <brovinSV80@gmail.com>
2023-02-05 23:02:11 +0200

    Add user_answer

--- py_charm.py
+++ py_charm.py
@@ -3,3 +3,5 @@ import random
 def mane_function():
     num1 = random.randint(5, 20)
     num2 = random.randint(5, 20)
+    print(num1, " + ", num2, " = ")
+    user_answer = int(input("Your answer: "))
